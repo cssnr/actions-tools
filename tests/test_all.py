bb2c0a3ded7f12d25265c575c21b40fb16c6af93
Shane <6071159+smashedr@users.noreply.github.com>
2025-02-20 22:36:37 -0800

    Testing

--- tests/test_all.py
+++ tests/test_all.py
@@ -10,7 +10,7 @@ def test_inputs():
     assert core.get_input("test") == os.environ["INPUT_TEST"].strip()
     assert core.get_input("test", low=True) == os.environ["INPUT_TEST"].strip().lower()
     assert core.get_input("test", strip=False) == os.environ["INPUT_TEST"]
-    assert core.get_input("test", boolean=True) == True
+    assert core.get_input("test", boolean=True)
     assert isinstance(core.get_input("test", split="\n"), list)
     assert len(core.get_input("test", split="\n")) == 1
 
